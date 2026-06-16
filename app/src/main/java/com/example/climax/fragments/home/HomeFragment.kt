bd91dd5921f93f3fb1f0d06e1f50556dfb21de53
Vikram Sahni <kumarvikram9356@gmail.com>
2026-06-16 17:50:03 +0530

    Location layout setup complete

--- app/src/main/java/com/example/climax/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/climax/fragments/home/HomeFragment.kt
@@ -4,21 +4,55 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.climax.data.CurrentLocation
+import com.example.climax.data.WeatherData
 import com.example.climax.databinding.FragmentHomeBinding
 import com.example.climax.databinding.FragmentHomeBinding.inflate
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = requireNotNull(_binding)
+
+    private val weatherDataAdapter = WeatherDataAdapter(
+        onLocationClicked = {
+            Toast.makeText(requireContext(), "onLocationClicked()", Toast.LENGTH_SHORT).show()
+        }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun setWeatherDataAdapter() {
+        binding.weatherDataRecyclerView.adapter = weatherDataAdapter
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setWeatherDataAdapter()
+        setWeatherData()
+    }
+
+    private fun setWeatherData() {
+        weatherDataAdapter.setData(listOf(CurrentLocation(getCurrentData())))
+    }
+
+    private fun getCurrentData(): String {
+        val currentDate = Date()
+        val formatter = SimpleDateFormat("d MMMM yyyy", Locale.getDefault())
+        return "Today, ${formatter.format(currentDate)}"
+
     }
 }
